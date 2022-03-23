fbe05f143e88d807a115127ca332eb8042106d32
Maria.Aristizabal <Maria.Aristizabal>
2022-03-23 14:08:56 +0000

    Add if statements to have the capability of reading the ocean initial condition files from MOM6 with the extension .a or .a.tgz

--- scripts/exhafs_ocn_prep.py
+++ scripts/exhafs_ocn_prep.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 
-import os, sys, logging
+import os, sys, logging, glob
 
 if 'USHhafs' in os.environ:
     sys.path.append(os.environ['USHhafs'])
@@ -53,26 +53,37 @@ os.system('mkdir ' + dir_hycominit1)
 os.system('mkdir ' + dir_hycominit) 
 
 # Linking MOM6 initial condition files to hycominit1 folder 
+file_restart_a = glob.glob(os.path.join(dir_mom6,'mom6_hat10*restart.a*'))[0].split('/')[-1]
+file_restart_b = glob.glob(os.path.join(dir_mom6,'mom6_hat10*restart.b'))[0].split('/')[-1]
+ext_restart_a = file_restart_a[-1]
+ext_archv_a = glob.glob(os.path.join(dir_mom6,'mom6_hat10*archv.a*'))[0][-1]
 if hour_int == 0:
-    cmd = 'ln -sf ' + dir_mom6 + '/' + 'mom6_hat10.t00z.n00.restart.a.tgz ' + dir_hycominit1 + '/' + 'mom6_hat10.t00z.n00.restart.a.tgz' 
+    cmd = 'ln -sf ' + dir_mom6 + '/' + file_restart_a + ' ' + dir_hycominit1 + '/' +  file_restart_a
     os.system(cmd)
-    cmd = 'ln -sf ' + dir_mom6 + '/' + 'mom6_hat10.t00z.n00.restart.b ' + dir_hycominit1 + '/' + 'mom6_hat10.t00z.n00.restart.b' 
+    cmd = 'ln -sf ' + dir_mom6 + '/' + file_restart_b + ' ' + dir_hycominit1 + '/' +  file_restart_b
     os.system(cmd)
-    os.chdir(dir_hycominit1)
-    cmd = 'tar -xpvzf ' + 'mom6_hat10.t00z.n00.restart.a.tgz' 
-    os.system(cmd)
-    cmd = 'cp ' + 'mom6.' + YMD + '/mom6_hat10.t12z.n00.restart.a ' + 'mom6_hat10.t00z.n00.restart.a' 
-    os.system(cmd)
+    if ext_restart_a != 'a':
+        os.chdir(dir_hycominit1)
+        cmd = 'tar -xpvzf ' + file_restart_a
+        os.system(cmd)
+        cmd = 'cp ' + 'mom6.' + YMD + '/mom6_hat10.t12z.n00.restart.a ' + 'mom6_hat10.t00z.n00.restart.a' 
+        os.system(cmd)
 else:
-    cmd = 'ln -sf ' + dir_mom6 + '/' + 'mom6_hat10.t00z.f' + hour + '.archv.a.tgz ' + dir_hycominit1 + '/' + 'mom6_hat10.t00z.f' + hour + '.archv.a.tgz' 
-    os.system(cmd)
-    cmd = 'ln -sf ' + dir_mom6 + '/' + 'mom6_hat10.t00z.f' + hour + '.archv.b ' + dir_hycominit1 + '/' + 'mom6_hat10.t00z.f' + hour + '.archv.b' 
-    os.system(cmd)
-    os.chdir(dir_hycominit1)
-    cmd = 'tar -xpvzf ' + 'mom6_hat10.t00z.f' + hour + '.archv.a.tgz' 
-    os.system(cmd)
-    cmd = 'cp ' + 'mom6.' + YMD + '/mom6_hat10.t12z.n00.restart.a ' + 'mom6_hat10.t00z.f' + hour + '.archv.a' 
-    os.system(cmd)
+    if ext_archv_a == 'a':
+        cmd = 'ln -sf ' + dir_mom6 + '/' + 'mom6_hat10.t00z.f' + hour + '.archv.a ' + dir_hycominit1 + '/' + 'mom6_hat10.t00z.f' + hour + '.archv.a' 
+        os.system(cmd)
+        cmd = 'ln -sf ' + dir_mom6 + '/' + 'mom6_hat10.t00z.f' + hour + '.archv.b ' + dir_hycominit1 + '/' + 'mom6_hat10.t00z.f' + hour + '.archv.b' 
+        os.system(cmd)
+    else:
+        cmd = 'ln -sf ' + dir_mom6 + '/' + 'mom6_hat10.t00z.f' + hour + '.archv.a.tgz ' + dir_hycominit1 + '/' + 'mom6_hat10.t00z.f' + hour + '.archv.a.tgz' 
+        os.system(cmd)
+        cmd = 'ln -sf ' + dir_mom6 + '/' + 'mom6_hat10.t00z.f' + hour + '.archv.b ' + dir_hycominit1 + '/' + 'mom6_hat10.t00z.f' + hour + '.archv.b' 
+        os.system(cmd)
+        os.chdir(dir_hycominit1)
+        cmd = 'tar -xpvzf ' + 'mom6_hat10.t00z.f' + hour + '.archv.a.tgz' 
+        os.system(cmd)
+        cmd = 'cp ' + 'mom6.' + YMD + '/mom6_hat10.t12z.n00.restart.a ' + 'mom6_hat10.t00z.f' + hour + '.archv.a' 
+        os.system(cmd)
 
 # Writing hycom_settings file
 os.chdir(dir_hycominit)
@@ -80,22 +91,25 @@ hycominit1.select_domain(logger)
 hycominit1.hycom_settings.deliver(frominfo='./hycom_settings')
 
 # Linking restart files to hycominit1 folder 
-cmd = 'ln -sf ' + dir_mom6 + '/' + 'mom6_hat10.t00z.n00.restart.a.tgz ' + dir_hycominit1 + '/' + 'mom6_hat10.t00z.n00.restart.a.tgz' 
+file_restart_a = glob.glob(os.path.join(dir_mom6,'mom6_hat10*restart.a*'))[0].split('/')[-1]
+file_restart_b = glob.glob(os.path.join(dir_mom6,'mom6_hat10*restart.b'))[0].split('/')[-1]
+ext_restart_a = file_restart_a[-1]
+cmd = 'ln -sf ' + dir_mom6 + '/' + file_restart_a + ' ' + dir_hycominit1 + '/' +  file_restart_a
 os.system(cmd)
-cmd = 'ln -sf ' + dir_mom6 + '/' + 'mom6_hat10.t00z.n00.restart.b ' + dir_hycominit1 + '/' + 'mom6_hat10.t00z.n00.restart.b' 
+cmd = 'ln -sf ' + dir_mom6 + '/' + file_restart_b + ' ' + dir_hycominit1 + '/' +  file_restart_b
 os.system(cmd)
 
 # copying restart files to hycominit folder 
-os.chdir(dir_hycominit1)
-cmd = 'tar -xpvzf ' + 'mom6_hat10.t00z.n00.restart.a.tgz' 
+cmd = 'cp ' + dir_hycominit1 + '/' + file_restart_a + ' ' + dir_hycominit + '/restart_out.a' 
 os.system(cmd)
-cmd = 'cp ' + 'mom6.' + YMD + '/mom6_hat10.t12z.n00.restart.a ' + dir_hycominit + '/restart_out.a' 
+cmd = 'cp ' + dir_hycominit1 + '/' + file_restart_b + ' ' + dir_hycominit + '/restart_out.b' 
 os.system(cmd)
-cmd = 'cp ' + dir_hycominit1 + '/mom6_hat10.t00z.n00.restart.b ' + dir_hycominit + '/restart_out.b' 
-os.system(cmd)
-
-
-#hycominit1.rtofs_spin(logger)
+if ext_restart_a != 'a':
+    os.chdir(dir_hycominit1)
+    cmd = 'tar -xpvzf ' + file_restart_a
+    os.system(cmd)
+    cmd = 'cp ' + 'mom6.' + YMD + '/mom6_hat10.t12z.n00.restart.a ' + dir_hycominit + '/restart_out.a'
+    os.system(cmd)
 
 logger.info("hycominit1 done")
 
